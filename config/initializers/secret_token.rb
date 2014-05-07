623092bcc3ffa3fe2fe023053ad408814a41f4aa
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-05-07 09:47:06 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ventacombustion::Application.config.secret_key_base = 'a0fe782d1c7d08c5673f96a87fbf4cbbfe7defdc266b78106d8a1763bafb199a765744b84b398feb350920ddc3fe32c93d40b678d755e3c80fa707fcab96c3af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ventacombustion::Application.config.session_store :cookie_store, key: '_ventacombustion_session'
