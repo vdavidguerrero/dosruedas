40198b8109c21c054398941c7f6e426e328a57c4
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-05-13 17:34:43 -0400

    auto Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-   City.create(name:"Santo Domigno")
-   City.create(name:"Azua")
-   City.create(name:"Santiago")
-   City.create(name:"Pedernales")
-   City.create(name:"San Francisco")
-   City.create(name:"San Juan")
+   #City.create(name:"Santo Domigno")
+   #City.create(name:"Azua")
+   #City.create(name:"Santiago")
+   #City.create(name:"Pedernales")
+   #City.create(name:"San Francisco")
+   #City.create(name:"San Juan")
+
+   #Brand.create(name:"yamaha")
+   #Brand.create(name:"Honda")
+   #Brand.create(name:"CanCan")
+   #
+   #Model.create(name:"R1",     brand_id:1)
+   #Model.create(name:"R6",     brand_id:1)
+   #Model.create(name:"Shadow", brand_id:2)
+   #
+   #Type.create(name:"Motocicleta")
+   #Type.create(name:"ATV")
+   #Type.create(name:"Acuativo")
+
+   Style.create(name:"Cruisier", description:"Motor1", photo_path:"/", type_id:1)
+   Style.create(name:"SportBike", description:"Motor1", photo_path:"/", type_id:1)
+
+   Vehicle.create(style_id:1, model_id:1)
+   Vehicle.create(style_id:1, model_id:2)
