a9aa0b65010fa39eeaa632684c0ba28c0403e792
Vincent David Guerrero Peña <v.davidguerrero@gmail.com>
2014-05-13 17:40:21 -0400

    auto Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@
    #City.create(name:"San Francisco")
    #City.create(name:"San Juan")
 
-   #Brand.create(name:"yamaha")
-   #Brand.create(name:"Honda")
-   #Brand.create(name:"CanCan")
-   #
-   #Model.create(name:"R1",     brand_id:1)
-   #Model.create(name:"R6",     brand_id:1)
-   #Model.create(name:"Shadow", brand_id:2)
-   #
-   #Type.create(name:"Motocicleta")
-   #Type.create(name:"ATV")
-   #Type.create(name:"Acuativo")
+   Brand.create(name:"yamaha")
+   Brand.create(name:"Honda")
+   Brand.create(name:"CanCan")
+
+   Model.create(name:"R1",     brand_id:1)
+   Model.create(name:"R6",     brand_id:1)
+   Model.create(name:"Shadow", brand_id:2)
+
+   Type.create(name:"Motocicleta")
+   Type.create(name:"ATV")
+   Type.create(name:"Acuativo")
 
    Style.create(name:"Cruisier", description:"Motor1", photo_path:"/", type_id:1)
    Style.create(name:"SportBike", description:"Motor1", photo_path:"/", type_id:1)
